2cc0ff9ac707ed10bcd2f1b0e8758d093a8df31e
su <2296887348@qq.com>
2020-11-14 13:45:14 +0800

    配置logback日志与多环境profile

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.soft1851.user.controller;
 
 import com.soft181.api.controller.user.HelloControllerApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  **/
 @RestController
+@Slf4j
 public class HelloController implements HelloControllerApi {
     @Override
-    public Object hello(){
+    public Object hello() {
+        log.info("info:hello");
+        log.warn("warn:hello");
+        log.error("error: hello");
         return "hello";
     }
 }
