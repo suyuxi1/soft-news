affb0fb5ffdfe0d214328d7b4336c857555ff9e1
su <2296887348@qq.com>
2020-11-14 14:13:23 +0800

    优雅地返回封装结果

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.soft1851.user.controller;
 
 import com.soft181.api.controller.user.HelloControllerApi;
+import com.soft1851.result.GraceResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,6 @@ public class HelloController implements HelloControllerApi {
     @Override
     public Object hello() {
         log.info("info:hello");
-        log.warn("warn:hello");
-        log.error("error: hello");
-        return "hello";
+        return GraceResult.ok("hello");
     }
 }
