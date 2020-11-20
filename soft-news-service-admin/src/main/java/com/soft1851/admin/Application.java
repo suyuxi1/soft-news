3ac34d722ac2c96339ada3b9d89db5614419b552
su <2296887348@qq.com>
2020-11-20 16:11:36 +0800

    admin模块准备

---
+++ soft-news-service-admin/src/main/java/com/soft1851/admin/Application.java
@@ -0,0 +1,20 @@
+package com.soft1851.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author su
+ * @className Application
+ * @Description admin模块启动类
+ * @Date 2020/11/20
+ * @Version 1.0
+ **/
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ soft-news-service-admin/src/main/java/com/soft1851/admin/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.soft1851.admin.controller;
+
+/**
+ * @author su
+ * @className HelloController
+ * @Description TODO
+ * @Date 2020/11/20
+ * @Version 1.0
+ **/
+public class HelloController {
+
+}
