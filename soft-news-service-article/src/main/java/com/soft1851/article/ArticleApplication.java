bbedd505a45f3500363394a638de0eef4ef1d1f9
su <2296887348@qq.com>
2020-11-24 14:21:02 +0800

    文章服务模块准备

---
+++ soft-news-service-article/src/main/java/com/soft1851/article/ArticleApplication.java
@@ -0,0 +1,20 @@
+package com.soft1851.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author su
+ * @className ArticleApplication
+ * @Description Article模块启动主类
+ * @Date 2020/11/24
+ * @Version 1.0
+ **/
+
+@SpringBootApplication
+public class ArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
